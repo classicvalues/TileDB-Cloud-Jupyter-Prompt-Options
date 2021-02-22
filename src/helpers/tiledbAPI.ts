e956740d62d8de461a6a1ae512c9ad033c906134
Konstantinos Sarantopoulos <sarantopouloskon@gmail.com>
2021-02-22 16:33:00 +0200

    Add version of api in basePath

--- src/helpers/tiledbAPI.ts
+++ src/helpers/tiledbAPI.ts
@@ -1,11 +1,13 @@
 import { UserApi } from '@tiledb-inc/tiledb-cloud';
 import { requestAPI } from './handler';
 
+const API_VERSION = 'v1';
+
 const getTileDBAPI = async (): Promise<UserApi> => {
   const data: any = await requestAPI();
   const config = {
     apiKey: data.token,
-    basePath: data.api_host,
+    basePath: `${data.api_host}/${API_VERSION}`,
   };
   return new UserApi(config);
 };
